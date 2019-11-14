de32f6d7de34d79ae70565a2baa7969707828d42
Pablo Orviz <orviz@ifca.unican.es>
2019-11-14 18:32:10 +0100

    Fix PEP8 errors/warnings

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Spanish National Research Council (CSIC)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 from setuptools import setup
 
 setup(
@@ -9,14 +23,14 @@ setup(
     author_email='orviz@ifca.unican.es',
     license='Apache 2.0',
     packages=['bulksend2cmdb'],
-    package_dir={'bulksend2cmdb':'bulksend2cmdb'},
+    package_dir={'bulksend2cmdb': 'bulksend2cmdb'},
     install_requires=[
         'requests',
         'simplejson',
         'six',
     ],
     zip_safe=False,
-    entry_points ={
+    entry_points={
         'console_scripts': ['bulksend2cmdb=bulksend2cmdb.main:main']
     }
 )
